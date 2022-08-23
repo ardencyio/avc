1ce85ce70a244f8cbc44ca1f01665c2403e06cbf
Iheanacho Ebere <eiheanacho52@gmail.com>
2022-08-23 14:43:03 +0100

    Footer component

---
+++ avc/src/lib/footer/footer-text.component.svelte
@@ -0,0 +1,34 @@
+<script>
+    export let type = false;
+
+    import linkedin from '../../assets/linkedin.logo.svg';
+    import ig from '../../assets/ig.logo.svg';
+    import twitter from '../../assets/twitter.logo.svg';
+
+</script>
+
+<div  class="w-9/12 text-primary mx-auto py-8 flex justify-between" class:type={type}>
+    <div class="text-sm space-y-4">
+        <p class="font-bold text-xl font-comfortaa">ardency</p>
+        <p class="font-Plus max-w-[208px]">We help you become a better startup, with better outcomes.</p>
+        <p class="font-Plus font-semibold">All right reserved © Ardency 2022</p>
+    </div>
+    <div class="w-[360px] font-Plus text-primary" class:type={type}>
+        <div class="flex justify-between py-3 border-t border-bd">
+            <p>Email</p>
+            <p class="font-bold">hello@audency.vc</p>
+        </div>
+        <div class="flex justify-between py-3 border-t border-bd">
+            <p>Location</p>
+            <p class="font-bold">London | Nigeria | Mauritius</p>
+        </div>
+        <div class="flex justify-between py-3 border-t border-bd">
+            <p>Socials</p>
+            <div class="flex gap-4">
+                <img src={twitter} alt="">
+                <img src={linkedin} alt="">
+                <img src={ig} alt="">
+            </div>
+        </div>
+    </div>
+</div>
--- avc/src/lib/footer/footer.svelte
+++ avc/src/lib/footer/footer.svelte
@@ -1,31 +1,12 @@
 <script>
     import bg from '../../assets/footer.map.svg';
+    import FooterText from './footer-text.component.svelte';
 </script>
 
 <div class="relative h-[360px] bg-bg-nav">
      <img class="absolute top-0 z-0 object-cover w-full" src={bg} alt="">
     
     <div class="absolute h-full w-full flex justify-center items-center">
-        <div  class="w-9/12 text-primary mx-auto py-8 flex justify-between">
-            <div class="text-sm space-y-4">
-                <p class="font-bold text-xl font-comfortaa">ardency</p>
-                <p class="font-Plus max-w-[208px]">We help you become a better startup, with better outcomes.</p>
-                <p class="font-Plus font-semibold">All right reserved © Ardency 2022</p>
-            </div>
-            <div class="w-[360px] font-Plus text-primary">
-                <div class="flex justify-between py-3 border-t border-bd">
-                    <p>Email</p>
-                    <p class="font-bold">hello@audency.vc</p>
-                </div>
-                <div class="flex justify-between py-3 border-t border-bd">
-                    <p>Location</p>
-                    <p class="font-bold">London | Nigeria | Mauritius</p>
-                </div>
-                <div class="flex justify-between py-3 border-t border-bd">
-                    <p>Socials</p>
-                    <p></p>
-                </div>
-            </div>
-        </div>
+        <FooterText />
     </div>
 </div>
