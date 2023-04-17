863ed83521a9804e3e3c2888f98aaba462127cbd
Pepeye <Pepeye@users.noreply.github.com>
2023-04-17 12:04:06 +0100

    x: removes cloudflare adaptor
    - improves tagline
    - removes cloudflare npm package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import cloudflare from "@astrojs/cloudflare";
-
-// https://astro.build/config
-export default defineConfig({
-  output: "server",
-  adapter: cloudflare()
-});
+export default defineConfig({});
